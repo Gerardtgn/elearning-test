8ff283e2d4efc49b4f4573b84d85e014a3b56ab6
Gerard TOGNY <gerardtgn276@gmail.com>
2025-02-25 20:47:46 +0100

    reparation erreur vercel

--- src/components/pages/ServicePage.jsx
+++ src/components/pages/ServicePage.jsx
@@ -2,7 +2,7 @@ import Navbar from "../layout/Navbar";
 import Footer from "../layout/Footer";
 import Titre from "../layout/Titre";
 import Testimonial from "../layout/Testimonial";
-import Service from "../layout/service";
+import Service from "../layout/Service";
 export default function ServicePage(){
     return(
         <>
